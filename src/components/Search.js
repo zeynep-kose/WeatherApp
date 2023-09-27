0ebc12bff4da94ac4a117cd3cf468ad95cb98cbf
Zeynep Köse <zynpk1903@gmail.com>
2023-09-27 11:28:08 +0300

    first commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,42 +1,39 @@
 import React from "react";
+import * as yup from "yup";
 import CitiesData from "../mocks/Cities";
-import {
-  Autocomplete,
-  Stack,
-  Card,
-  Box,
-  CardContent,
-  Icon,
-  Typography,
-  CardMedia,
-} from "@mui/material";
+import { Autocomplete, Stack, Card, Box } from "@mui/material";
 import TextField from "@material-ui/core/TextField";
-import ValidationSchema from "./validation.js";
-import { useFormik, Error } from "formik";
+import { useFormik } from "formik";
+
+const ValidationSchema = yup.object().shape({
+  city: yup.string().trim().required("City is required"),
+});
+
 function Search({ city, setCity }) {
   const formik = useFormik({
     initialValues: {
       city: "Bursa",
     },
-    ValidationSchema,
+    validationSchema: ValidationSchema,
     onSubmit: (values) => {
       console.log("Form submitted with values:", values);
 
       setCity(values.city);
     },
   });
+  console.log("theDamnErros:", formik.errors);
 
   return (
     <Stack className="leftPart">
       <Box sx={{ width: "100%" }}>
-        <form>
+        <form onSubmit={formik.handleSubmit}>
           <Autocomplete
             disablePortal
             id="combo-box-demo"
             options={CitiesData}
             value={formik.values.city}
             sx={{ width: 300 }}
-            name="cittyChange"
+            name="cityChange"
             onChange={(event, value) => {
               formik.setFieldValue("city", value);
               setCity(value);
